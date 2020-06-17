dfa175d62318f1e2addd30ae3e66ddfebf44d77e
sansheng <807156237@qq.com>
2020-06-17 09:21:56 +0800

    string

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
-
+sentence = sentence.toUpperCase();
+sentence = sentence.replace('MR MIKE','Mr Mike');
+console.log(sentence);
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.substr(1);
+console.log(money);
